b54bfd9d801c428e1a08afde4fe7da028b9bb71a
이정배 <jblee@bankbe.net>
2023-07-26 15:34:20 +0900

    Add: iframe 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>안녕하세요. 버셀 배포 테스트입니다.</h1>
       <p>변경1</p>
+      <iframe src="https://innerpage-iframe.vercel.app/" frameborder="0"></iframe>
     </div>
   );
 }
