918bb1341efdef876cece13d4341d750950f07dd
이정배 <jblee@bankbe.net>
2023-07-27 14:31:28 +0900

    Add: 링크 주소 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>안녕하세요. 버셀 배포 테스트입니다.</h1>
       <p>변경1</p>
       <iframe src="https://innerpage-iframe.vercel.app/" frameborder="0"></iframe>
-      <a href="http://localhost/user/sign-up">하이픈 가입페이지로</a>
+      <a href="https://market.bankbe.im/user/sign-up">하이픈 가입페이지로</a>
     </div>
   );
 }
