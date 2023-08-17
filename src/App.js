b87f35e47c2fda0782fedc6275b7e268120dc3da
이정배 <jblee@bankbe.net>
2023-08-17 14:01:29 +0900

    Add: jsessionId ifrme 사이트에 주기

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import {Link, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import Sub from "./pages/Sub";
 import ReactGA from 'react-ga4';
+import React, { useRef } from 'react';
 
 
 function App() {
+  const iframeRef = useRef();
+  const sendJSessionId = () => {
+    const jsessionId = 'testtest11';
+    const iframeWindow = iframeRef.current.contentWindow;
+
+    // 메시지 보내기
+    iframeWindow.postMessage(jsessionId, 'https://innerpage-iframe.vercel.app');
+  };
   return (
     <div className="App">
       <GoogleTagManager gtmId="GTM-T22M3WCP" />
       <h1>안녕하세요. 버셀 배포 테스트입니다.</h1>
       <p>변경1</p>
-      <iframe src="https://innerpage-iframe.vercel.app/" frameborder="0"></iframe>
+      <iframe ref={iframeRef} src="https://innerpage-iframe.vercel.app/" frameborder="0"></iframe>
+      <button onClick={sendJSessionId}>Send JSessionID</button>
       <a href="https://market.bankbe.im/user/sign-up">하이픈 가입페이지로</a>
       <nav style={{display:'flex', justifyContent: 'center', border: '1px solid black', padding: '10px', marginTop: '50px'}}>
         <div style={{width: '50%', display: 'flex', justifyContent: 'space-between'}}>
